59c3796daceabe512653bae4afb6003068ae3182
linggew <56491864+linggew@users.noreply.github.com>
2022-06-10 01:36:44 -0700

    Update Qtable.java

--- Algorithm1_Qlearning/Qtable.java
+++ Algorithm1_Qlearning/Qtable.java
@@ -1,5 +1,5 @@
-package AI;
-
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Dictionary;
@@ -7,10 +7,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Qtable {
-	static int episodes = 500000;// since the snake will resets itself after death, so my 1 epsode here doesn't mean one game, but one move of the snake.
+	static int episodes = 5000000;
 	static double learning_rate = 0.2;
-	static double gamma = 0.9;
+	static double gamma = 0.8;
 	static double epsilon_Greedy = 0.2;
+	static int []score_plot=new int [501];
+	static int point=0;
+	static int history_highest_score=0;
 	static HashMap<ArrayList<Integer>,ArrayList<Double>> Qtable=new HashMap <ArrayList<Integer>,ArrayList<Double>>();
 	
 	public static int[][] env() {
@@ -23,22 +26,43 @@ public class Qtable {
 			}
 		return env;
 	}
-
+	
+	private static void exportData(int[] rewardPlot, String filename){
+	    File file = new File(filename+".txt");
+	    file.delete();
+	    try {
+	        file.createNewFile();
+	        PrintWriter out = new PrintWriter(filename+".txt");
+	        StringBuilder string = new StringBuilder();
+	        for(int i=0;i<rewardPlot.length;i++){
+	            string.append(rewardPlot[i]);
+	            string.append("\n");
+	            out.write(string.toString());
+	            string.setLength(0);
+	        }
+	        out.close();
+	    }
+	    catch(Exception e) {
+	        System.out.println("File output failure");
+	        return;
+	    }
+	}
+	
 	public static void main(String[] args) {
 		int[][] world= env();
 		//ArrayList<Double> zero_list = new ArrayList<Double>() {{add((double) 0);add((double) 0);add((double) 0);add((double) 0);add((double) 0);}};
 		Robot_snake robot = new Robot_snake(world);
-		for(int time=0; time<episodes;time++) {
+		for(int time=0; time<episodes+1;time++) {
 				ArrayList<Integer> current_state=robot.state(world);
-				boolean contains = Qtable.containsKey(current_state);// if the Qtable contain the current state, if not, add the pair(current_state)- value[0,0,0,0,0]
+				boolean contains = Qtable.containsKey(current_state);
 				if(!contains) {
 					ArrayList<Double> zero_list = new ArrayList<Double>() {{add((double) 0);add((double) 0);add((double) 0);add((double) 0);add((double) 0);}};
 					Qtable.put(current_state,zero_list);
 				}
-				int act=robot.choose_action(current_state, Qtable,epsilon_Greedy);//choose the max value of current state in Qtable as act
-				int awards=robot.award(act,world);//get awards for current act
+				int act=robot.choose_action(current_state, Qtable,epsilon_Greedy);
+				int awards=robot.award(act,world);
 				ArrayList<Integer> next_state=robot.state(world);
-				contains = Qtable.containsKey(next_state);//if the Qtable contain the next state, if not add to the qtable
+				contains = Qtable.containsKey(next_state);
 				if(!contains) {
 					ArrayList<Double> zero_list = new ArrayList<Double>() {{add((double) 0);add((double) 0);add((double) 0);add((double) 0);add((double) 0);}};
 					Qtable.put(next_state,zero_list);
@@ -53,14 +77,21 @@ public class Qtable {
 				Qtable_current_state_actions.set(act,Qtable_current_action_updated);
 				Qtable.put(current_state,Qtable_current_state_actions);
 //*******************************************************************************************************	
+				if(time%10000==0) {
+					if(epsilon_Greedy>0) epsilon_Greedy=epsilon_Greedy-0.001;
+					score_plot[point]=robot.history_score;
+					point=point+1;
+					if(robot.history_score>history_highest_score) history_highest_score=robot.history_score;
+					robot.history_score=0;
+				}
 			}
-		for (ArrayList<Integer> i : Qtable.keySet()) //print qtable
-      			System.out.println("key: " + i + " value: " + Qtable.get(i));
-		for (int i=0;i<12;i++) {	//print maze
-			System.out.print("\n");
-			for(int j=0;j<12;j++)
-				System.out.print(world[i][j]);}
-		System.out.println("\nthe highest score is"+robot.history_score);// print highest history score
-	
+		exportData(score_plot,"Plot");
+//		for (ArrayList<Integer> i : Qtable.keySet()) 
+//      System.out.println("key: " + i + " value: " + Qtable.get(i));
+
+		System.out.println("\nthe highest score is"+history_highest_score);
+		System.out.println("\nuse google sheet to draw the plot with Plot.txt doc");
+//	
 	}
+//	
 }
